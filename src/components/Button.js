0aece413acaa69f599cdc69d861d8fd19ae52c63
bright okike <brightokike@gmail.com>
2020-01-06 21:33:23 +0300

    changes after review

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/button-has-type */
-/* eslint-disable indent, no-tabs */
+/* eslint-disable indent, no-tabs, no-mixed-spaces-and-tabs */
 
 import React from 'react';
 import PropTypes from 'prop-types';
