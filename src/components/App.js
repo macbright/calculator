db1230c3211c781665c70e0cdef41e87d6db9da7
bright okike <brightokike@gmail.com>
2020-01-09 11:28:06 +0300

    implemented the app logic that calculates the task

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
 class App extends React.Component {
   render() {
